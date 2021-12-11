5c0a4aef168ed80511ff071711613c5755d21ce3
Lucas Maia <lcs_maia@live.com>
2021-12-11 17:26:38 -0300

    Fix: ajuste no elemento body

--- cypress/support/pages/articles/elements.js
+++ cypress/support/pages/articles/elements.js
@@ -2,6 +2,6 @@ export const ELEMENTS = {
   linkNovoArtigo: '[href*=editor]',
   inputTitle: 'input[ng-model$=title]',
   inputDescription: 'input[ng-model$=description]',
-  inputBody: 'input[ng-model$=body]',
+  inputBody: 'textarea[ng-model$=body]',
   inputTags: 'input[ng-model$=tagField]'
 }
